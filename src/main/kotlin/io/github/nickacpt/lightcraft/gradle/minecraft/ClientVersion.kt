cea7213da2cc96d9ae613827dd2eff15dd1bac97
SirObsidian <70753342+SirObby@users.noreply.github.com>
2021-09-13 20:20:20 +0300

    1.17.1 Support

--- src/main/kotlin/io/github/nickacpt/lightcraft/gradle/minecraft/ClientVersion.kt
+++ src/main/kotlin/io/github/nickacpt/lightcraft/gradle/minecraft/ClientVersion.kt
@@ -10,5 +10,6 @@ enum class ClientVersion(
     V1_5_2("1.5.2", "net.minecraft.client.Minecraft", hasLegacyAssets = true, hasExtraMappings = true),
     V1_6_4("1.6.4", "net.minecraft.client.main.Main", "net.minecraft.client.Minecraft"),
     V1_7_10("1.7.10", "net.minecraft.client.main.Main", "net.minecraft.client.Minecraft"),
-    V1_8_9("1.8.9", "net.minecraft.client.main.Main", "net.minecraft.client.Minecraft")
+    V1_8_9("1.8.9", "net.minecraft.client.main.Main", "net.minecraft.client.Minecraft"),
+    V1_8_9("1.17.1", "net.minecraft.client.main.Main", "net.minecraft.client.Minecraft")
 }
